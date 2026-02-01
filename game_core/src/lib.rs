c68a8eac7096e803743f4fca4e3c67b71040d9ff
casp-dev <caspar@jung.bio>
2026-02-01 16:57:23 +0100

    [DIOXUS]: add dioxusgame trait

--- game_core/src/lib.rs
+++ game_core/src/lib.rs
@@ -1,6 +1,14 @@
+use dioxus::prelude::*;
 use egui::Ui;
 
+/// legacy trait für EGUI
 pub trait Game {
     fn name(&self) -> &str;
     fn ui(&mut self, ui: &mut Ui);
+}
+
+/// neuer trait für dioxus-spiele
+pub trait DioxusGame {
+    fn name(&self) -> &str;
+    fn render(&self) -> Element;
 }
